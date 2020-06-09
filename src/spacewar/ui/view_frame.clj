5f657e0298fbf7169067fdef65cbf8238c1d6695
Mario Viapiano <marioeviapiano@gmail.com>
2020-06-09 19:43:55 +0200

    implement star-field/draw

--- src/spacewar/ui/view_frame.clj
+++ src/spacewar/ui/view_frame.clj
@@ -2,11 +2,35 @@
   (:require [quil.core :as q]
             [spacewar.ui.protocols :as p]))
 
+(defn star-count [_])
+(defn star-size [_])
+(defn star-in-frame [_ _ _])
+(defn star-color [_])
+(defn move-stars [_])
+(defn make-stars [_])
+(defn add-stars [_])
+
 (deftype star-field [state]
   p/Drawable
-  (draw [this] this)
-  (setup [this] this)
-  (update-state [this] this))
+  (draw [_]
+    (let [{:keys [x y h w stars]} state]
+      (q/no-stroke)
+      (doseq [star stars]
+        (let [{:keys [h-distance v-distance luminosity angle]} star
+              v (* h (/ v-distance h-distance))
+              vx (* v (Math/cos angle))
+              vy (* v (Math/sin angle))
+              sx (+ vx x (/ w 2))
+              sy (+ vy y (/ h 2))
+              m (/ luminosity
+                   (Math/sqrt (+ (* h-distance h-distance) (* v-distance v-distance))))
+              sz (star-size m)]
+          (when (star-in-frame state sx sy)
+            (do
+              (apply q/fill (star-color m))
+              (q/ellipse sx sy sz sz)))))))
+  (setup [_] (star-field. (assoc state :stars (make-stars star-count))))
+  (update-state [_] (star-field. (add-stars (update state :stars move-stars)))))
 
 (deftype frame [state]
   p/Drawable
